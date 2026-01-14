a483fa21eca79c1f965aa4acea0bf45c3cdb9e57
jpk2805 <jp28280505@gmail.com>
2026-01-14 00:34:11 -0500

    implemented message fragment agent network

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,12 +1,13 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { helloWorld } from "@/inngest/functions";
+import { codeAgentFunction } from "@/inngest/functions";
 
 // Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld, 
+    codeAgentFunction
+    , 
   ],
 }
 );
