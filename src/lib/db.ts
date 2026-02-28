b9aef3b37adde6f13254638b44d05471021fb3d1
jpk2805 <jp28280505@gmail.com>
2026-02-28 03:01:16 -0500

    fixing issues

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from '../generated/prisma/client'
-import { neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
+import { PrismaNeonHttp } from '@prisma/adapter-neon'
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaNeonHttp(process.env.DATABASE_URL!, {})
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
   adapter,
